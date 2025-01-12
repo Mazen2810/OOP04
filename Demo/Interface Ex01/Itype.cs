d983b8632978cf0d80b34fdb79652a593047cf6b
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-12 11:24:33 +0200

    Interface - Ex01

---
+++ Demo/Interface Ex01/Itype.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Interface_Ex01
+{  // Developer ---> 1
+    internal interface Itype
+    {
+        // Public Access Modifier is the Default Access Modifier inside the Interface.
+        // Private Access Modifier is not Allowed for Signatures only [Property, Methods]
+
+
+        // what you can write inside the interface ?
+
+        // 1. Signature for property
+        public int MyProperty { get; set; }
+
+        // 2. Signature for method
+        public void MyMethod();
+
+        // 3. Default Implemented Method [C# 8.0 New feature (>NET Core 3.1 [2019])]
+        public void Print()
+        {
+            Console.WriteLine("Default Implemented Method");
+        }
+
+        /// // 4. Static Members [C# 8.0 New feature (>NET Core 3.1 [2019])]
+        ///
+        ///    // 4.1 Static Fields
+        ///    // 4.2 Static Properties
+        ///    // 4.3 Static Methods
+        ///    // 4.4 Static Events
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Demo
+﻿using Demo.Interface_Ex01;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+           // Itype reference;
+            // Declare for reference of type "Itype", containing garbage value
+            /// this Reference can refer to an Object of any type Implementing IType Interface
+            /// CLR will Allocate 4 unintialized Bytes in Stack for this reference
+            /// CLR will allocate 0 Bytes in HEAP
+
+
+            // reference = new Itype(); INVALID
+
+            //reference = new TypeA();
+            //reference.MyProperty = 10;
+            //reference.MyMethod();
+            //reference.Print();
+
+
+            //TypeA typeA = new TypeA();
+            //typeA.MyProperty = 20;
+            //typeA.MyMethod();
+            //typeA.Print():   // INVALID , can't see this method
+
         }
     }
 }
