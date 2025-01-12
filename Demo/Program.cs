b48130efeaa7eac3338609b0b1fd2f4decb2ebdd
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-12 14:00:25 +0200

    Interface Ex02

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,14 +1,32 @@
-﻿using Demo.Interface_Ex01;
+﻿
+using Demo.Interface_Ex02;
 
 namespace Demo
 {
     internal class Program
     {
+        #region Interface Ex02
+        static void PrintTenNUmbersFromSeries(ISeries series)
+        {
+            if (series == null)
+            {
+                return;
+            }
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.Write($"{series.Current} \t");
+                series.GetNext();
+            }
+            series.Reset();
+            Console.WriteLine();
+        } 
+        #endregion
         static void Main(string[] args)
         {
 
 
-           // Itype reference;
+            #region Interface Ex01
+            // Itype reference;
             // Declare for reference of type "Itype", containing garbage value
             /// this Reference can refer to an Object of any type Implementing IType Interface
             /// CLR will Allocate 4 unintialized Bytes in Stack for this reference
@@ -26,7 +44,20 @@ namespace Demo
             //TypeA typeA = new TypeA();
             //typeA.MyProperty = 20;
             //typeA.MyMethod();
-            //typeA.Print():   // INVALID , can't see this method
+            //typeA.Print():   // INVALID , can't see this method 
+            #endregion
+
+            #region Interface Ex02
+            //TypeA seriesByTwo = new TypeA();
+            //PrintTenNUmbersFromSeries(seriesByTwo);
+
+            //TypeB seriesByThree = new TypeB();
+            //PrintTenNUmbersFromSeries(seriesByThree);
+
+            //TypeC seriesByFour = new TypeC();
+            //PrintTenNUmbersFromSeries(seriesByThree); 
+            #endregion
+
 
         }
     }
